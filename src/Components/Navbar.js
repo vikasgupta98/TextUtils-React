c04e31b110b1e489d82137d80a350523741296cc
Vikas Gupta <vg9859@gmail.com>
2023-05-25 16:10:56 +0530

    version 2

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -17,7 +17,7 @@ export default function Navbar(props) {
                             <a className="nav-link active" aria-current="page" href="#">Home</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="/about">About</a>
+                            <a className="nav-link" href="#">About</a>
                         </li>
                     </ul>
                     {/* <form className="d-flex">
